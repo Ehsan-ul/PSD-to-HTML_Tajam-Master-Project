c20d7fc550443295dc0f7e0daf3fc209e54aab0d
Ehsanul Islam Sajjad <ehsanulislam82@gmail.com>
2021-08-08 01:58:10 +0600

    slick slider adding

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $('.banner-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 2000,
+  });
+});
