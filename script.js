9cf5baf6c52dee96458a60f565fecee8177612cc
Ehsanul Islam Sajjad <ehsanulislam82@gmail.com>
2021-08-08 02:52:45 +0600

    vedio part - venobox

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  // slick slider
   $('.banner-slider').slick({
     infinite: true,
     slidesToShow: 1,
@@ -7,6 +8,16 @@ $(document).ready(function () {
     dots: true,
     autoplay: true,
     autoplaySpeed: 2000,
-    speed: 2000,
+    speed: 3000,
+  });
+
+  // venobox
+  $('.venobox').venobox({
+    border: '10px', // default: '0'
+    bgcolor: '#377dff', // default: '#fff'
+    share: ['facebook', 'twitter', 'linkedin'], // default: []
+    spinner: 'rotating-plane',
+    spinColor: '#377dff',
+    overlayColor: '#34495e',
   });
 });
