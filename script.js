4ad8b30319ae32644aba250b8a3c00284df1ffda
Ehsanul Islam Sajjad <ehsanulislam82@gmail.com>
2021-08-09 03:19:01 +0600

    slick slider is added to team-two part

--- script.js
+++ script.js
@@ -11,16 +11,6 @@ $(document).ready(function () {
     speed: 3000,
   });
 
-  // venobox
-  $('.venobox').venobox({
-    border: '10px', // default: '0'
-    bgcolor: '#377dff', // default: '#fff'
-    share: ['facebook', 'twitter', 'linkedin'], // default: []
-    spinner: 'rotating-plane',
-    spinColor: '#377dff',
-    overlayColor: '#34495e',
-  });
-
   // team slick slider
   $('.team-slider').slick({
     infinite: true,
@@ -31,5 +21,35 @@ $(document).ready(function () {
     autoplay: true,
     autoplaySpeed: 2000,
     speed: 3000,
+  });
+
+  // team-two slider
+  $('.text-slide').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: false,
+    asNavFor: '.slider-img',
+  });
+  $('.slider-img').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.text-slide',
+    prevArrow: 'team-arrow-left',
+    nextArrow: 'team-arrow-right',
+    dots: false,
+    centerMode: true,
+    centerPadding: '0px',
+    focusOnSelect: true,
+  });
+
+  // venobox
+  $('.venobox').venobox({
+    border: '10px', // default: '0'
+    bgcolor: '#377dff', // default: '#fff'
+    share: ['facebook', 'twitter', 'linkedin'], // default: []
+    spinner: 'rotating-plane',
+    spinColor: '#377dff',
+    overlayColor: '#34495e',
   });
 });
